9fcaeb4ea62a8201fc92b16b4cbcf4470a32da3f
Vsevolod Cherepashkin <45619592+cherepas@users.noreply.github.com>
2022-01-20 12:04:22 +0100

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -14,8 +14,6 @@
 # For cpu cores, we have then $c=C/g=96/4 = 24$. Total number of available cores
 # $C$ on Booster is  2 * 24 * 2 = 96 threads.
 
-
-
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=4
 #SBATCH --output=./output/pmh-%j.out
@@ -25,10 +23,7 @@
 #SBATCH --gres=gpu:4
 
 #SBATCH --account=delia-mp
-
-#source /p/project/training2107/course2021_working_environment/activate.sh
 module load GCC/9.3.0 OpenMPI/4.1.0rc1 Python/3.8.5 Horovod/0.20.3-Python-3.8.5 PyTorch/1.7.0-Python-3.8.5 scikit/2020-Python-3.8.5 torchvision/0.8.2-Python-3.8.5
-#OpenMPI/4.1.0rc1 OpenMPI mpi4py TensorFlow
 HOROVOD_MPI_THREADS_DISABLE=0
 source /p/home/jusers/cherepashkin1/juwels/scalable_dl/sdl_venv/sdl_venv/activate.sh
 # make sure all GPUs on a node are visible
